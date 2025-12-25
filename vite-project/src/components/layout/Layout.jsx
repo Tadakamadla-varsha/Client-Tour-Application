196ecb9c740b524755730305ac1e87f8da94af4c
varsha <tadakamadlavarsha@gmail.com>
2025-12-25 20:19:27 +0530

    add header component with navigation UI

---
+++ vite-project/src/components/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../header/Header";
+
+
+
+
+const Layout= () => {
+  return (
+    <>
+      <Header/>
+     
+    </>
+  );
+};
+
+export default Layout;
